5873eab77e778ad683b985f7dfe1f5349670abe6
jerrydayne <jerrydaexplorer@gmail.com>
2023-03-16 15:28:25 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this is the primary function of the code
+ *
+ * Return: 0 shows it's always sucessful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
