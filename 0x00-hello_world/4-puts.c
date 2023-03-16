762e4c598cfa1f31c33be055a7ffd580f3d705a6
jerrydayne <jerrydaexplorer@gmail.com>
2023-03-16 15:16:27 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-/*
- * main - the primary function
- *
- * Return shows status, should always be 0 as success
- */
-
 #include <stdio.h>
 
-int main (void)
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
