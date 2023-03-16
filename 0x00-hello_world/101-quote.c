2d57962ff970326dd60ef404dc6ee3b0dd67fd7f
jerrydayne <jerrydaexplorer@gmail.com>
2023-03-16 16:44:29 -0700

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - the function that holds the sorce code
+ *
+ * Return: 0 as success but returns 1 here
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, stdout");
+	return (1);
+}
